091cae958c2d1e46e732b1d2bb75fe04f44ccd9b
connor johnson <connor.johnson@plenadata.com>
2019-09-20 12:20:12 -0600

    side projects

--- ScienceSimulators/physics/equations.py
+++ ScienceSimulators/physics/equations.py
@@ -1,5 +1,5 @@
-def equation1(finalVelocity, initialVelocity, accelaretion, time):
-    print(finalVelocity, initialVelocity, accelaration, time)
+def equation1(finalVelocity, initialVelocity, acceleration, time):
+    print(finalVelocity, initialVelocity, acceleration, time)
 
 def equaiton2(finalPosition, startPosition, initalVelocity, time, accelaration):
     print(finalPosition, startPosition, initalVelocity, time, accelaration)
