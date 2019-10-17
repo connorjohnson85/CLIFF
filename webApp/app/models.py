98ea03e454b99314ad63f6746e1f87166fcb11d5
connor johnson <connor.johnson@plenadata.com>
2019-10-17 09:47:16 -0600

    added command history table

--- webApp/app/models.py
+++ webApp/app/models.py
@@ -8,3 +8,11 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User {}>'.format(self.username)   
+
+class History(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True)
+    command = db.Column(db.String(120), index=True)
+
+    def __repr__(self):
+        return '<user {} command {}>'.format(self.username, self.command)
