7d118c483debf1f452a5ab605b1650173afa0ed8
connor johnson <connor.johnson@plenadata.com>
2019-11-02 20:19:12 -0600

    force and gravity

---
+++ ScienceSimulators/physics/forceGravity.py
@@ -0,0 +1,6 @@
+
+mass1 = input("Mass Of Object 1? ")
+mass2 = input("Mass of Object 1? ")
+radius = input("Distance? ")
+answer = (6.67*(10**-11) * float(mass1) * float(mass2))/(float(radius)**2)
+print(answer)
