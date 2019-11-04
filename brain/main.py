9306059acf8f8c67201b28466a8998fa7d35af18
connor johnson <connor.johnson@plenadata.com>
2019-11-04 12:00:18 -0700

    adding random byte bouncing

--- brain/main.py
+++ brain/main.py
@@ -1,3 +1,4 @@
+import random
 
 # my nueron model class. Expect it to change. A lot XD XD XD
 class node():
@@ -15,26 +16,35 @@ class node():
             connection2.name: 2,
             connection3.name: 3
             } 
-        self.binarySignal = 1
+        self.binarySignal = 0
 
     # connects with another nueron, "pinging" it with a signal
     def connect(self, id):
         name = self.name
         if id == 1:
-            self.connection1.sendConnectSignal(name)
+            response = self.connection1.sendConnectSignal(name)
         if id == 2:
-            self.connection2.sendConnectSignal(name)
+            response = self.connection2.sendConnectSignal(name)
         if id == 3: 
-            self.connection3.sendConnectSignal(name)
+            response = self.connection3.sendConnectSignal(name)
+        return response
 
     # sends data back when pinged. A single binary off or on signal
     def sendData(self, id):
-        return(self.binarySignal)
-    
+        if id == 1:
+            response = self.connection1.receiveData(self.name, self.binarySignal)
+        if id == 2:
+            response = self.connection2.receiveData(self.name, self.binarySignal)
+        if id == 3:
+            response = self.connection3.receiveData(self.name, self.binarySignal)
+        return response 
     # The ping class. Gets pinged, sends data back to the neuron that pinged it
     def sendConnectSignal(self, name):
-        self.sendData(id)
-        return 'connected'
+        response = self.sendData(self.getConnectionIDByName(name))
+        return response
+    def receiveData(self, name, data):
+        return name, data
+
 
     def getConnectionIDByName(self, name):
         id = self.connectionNames[name]
@@ -50,5 +60,29 @@ node2.setConnections(node1, node3, node4)
 node3.setConnections(node1, node2, node4)
 node4.setConnections(node1, node2, node3)
 
-hello = node1.connect(3)
+response = node1.connect(1)
+x = 0
+while x != 1000:
+    response = node1.connect(random.randint(1,3))
+    if response[1] == 0:
+        node1.binarySignal = 1
+        if response[0] == 'node2':
+            node2.binarySignal = 1
+        if response[0] == 'node3':
+            node3.binarySignal = 1
+        if response[0] == 'node4':
+            node4.binarySignal = 1
+    else:
+        node1.binarySignal = 0
+        if response[0] == 'node2':
+            node2.binarySignal = 0
+        if response[0] == 'node3':
+            node3.binarySignal = 0
+        if response[0] == 'node4':
+            node4.binarySignal = 0
+    x += 1 
 
+print(node1.binarySignal)
+print(node2.binarySignal)
+print(node3.binarySignal)
+print(node4.binarySignal)
