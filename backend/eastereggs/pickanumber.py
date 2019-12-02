e770b95cabaa9be8420e0e880fc056c990e3b63c
connor johnson <connor.johnson@plenadata.com>
2019-12-02 09:13:23 -0700

    refactoring protocols and pick a number .

--- backend/eastereggs/pickanumber.py
+++ backend/eastereggs/pickanumber.py
@@ -1,9 +1,7 @@
 import random
 from services.Speech import inputTesting, textTesting
 
-def pickANumber(mode):
-    start = inputTesting(mode, 'First number? ')
-    end = inputTesting(mode, 'Second number? ')
+def pickANumber(mode, start, end):
     number = random.randint(int(start), int(end))
     textTesting(mode, 'picking a number between {0} and {1}'.format(start, end))
     textTesting(mode, 'Your number is {0}'.format(number))
