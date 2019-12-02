2abc42773187cfb02a2db3f9a00fc5f74f10cd30
connor johnson <connor.johnson@plenadata.com>
2019-12-02 11:17:28 -0700

    adding basic unit conversion

---
+++ backend/commands/unitconversions.py
@@ -0,0 +1,37 @@
+import math
+
+def kilogramsTopounds(kilograms):
+    return(kilograms * 2.2046226)
+
+def kilogramsToOunces(kilograms):
+    return(kilograms * 35.273962)
+
+def kilogramsToTons(kilograms):
+    return(kilograms * 0.0011023113)
+
+def poundsToKilograms(pounds):
+    return(pounds * .45359237)
+
+def poundsToOunces(pounds):
+    return(pounds * 16)
+
+def poundsToTon(pounds):
+    return(pounds * 1/2000)
+
+def ouncesToKilograms(ounces):
+    return(ounces * 1/16 * .45359237)
+
+def ouncesToTons(ounces):
+    return(ounces * 1/16 * 1/2000)
+
+def ouncesToPounds(ounces):
+    return(ounces * 16)
+
+def tonsToKilograms(tons):
+    return(tons * 907.18474)
+
+def tonsToPounds(tons):
+    return(tons * 2000)
+
+def tonsToOunces(tons):
+    return(tonsToPounds(tons)*16)
