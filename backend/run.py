80d60c1ed5c97e94efd8d6e0481c62fff9b69c45
connor johnson <connor.johnson@plenadata.com>
2019-12-02 09:12:57 -0700

    adding last command

--- backend/run.py
+++ backend/run.py
@@ -7,20 +7,21 @@ def run(name, mode):
     AIOn = True
     if mode == 'production':
         ts('What do you need assistance with today?')
-
+        lastCommand = 'hello'
         while AIOn == True: 
             # Runs speech Recognition
             question = speechrecognition()
             AIOn = p.check(question)
-            p.processAnswer(mode, name, question)
+            lastCommand = p.processAnswer(mode, name, question, lastCommand)
             ts('command ran')
 
     if mode == 'silent':  
+        lastCommand = 'hello'
         print('What do you need assistance with today?')
 
         while AIOn == True:
             question = input(name + ': ')
             AIOn = p.check(question)
-            p.processAnswer(mode, name, question)
+            lastCommand = p.processAnswer(mode, name, question, lastCommand)
             print('command ran successfully')
 
